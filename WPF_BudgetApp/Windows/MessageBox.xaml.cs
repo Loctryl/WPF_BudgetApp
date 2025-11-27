86a0fba47c67258ef677870f5a84ea2d40f87b09
Thibault Le Tallec <letallec.thibault@gmail.com>
2025-11-27 10:49:06 +0100

    <ADD> Custom MessageBox

---
+++ WPF_BudgetApp/Windows/MessageBox.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace WPF_BudgetApp.Windows;
+
+public partial class MessageBox : Window
+{
+	public MessageBox(string message)
+	{
+		InitializeComponent();
+		Topmost = true;
+		Deactivated += Window_Deactivated;
+		
+		textDisplay.Text = message;
+	}
+	
+	private void Window_Deactivated(object? sender, EventArgs e)
+	{
+		Activate();
+		Focus();
+	}
+	
+	private void ConfirmButt_OnClick(object sender, RoutedEventArgs e) => Close();
+}
